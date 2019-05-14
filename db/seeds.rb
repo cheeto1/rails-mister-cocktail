df756538c0557300285b6ce6e8196d4c741ca1d3
Che To <chekinto@hotmail.com>
2019-05-14 18:17:36 +0100

    Mister Cocktail cocktail routes completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+results = JSON.parse(open(url).read)
+puts results
+
+results['drinks'].each do |drink|
+  Ingredient.create!(name: drink['strIngredient1'])
+  puts "#{drink}"
+end
+
+mojito = Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Martini')
+Cocktail.create(name: 'Long Island')
+
+Dose.create(description: "cocktail with mint", cocktail: mojito, ingredient: Ingredient.first)
